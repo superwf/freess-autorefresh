dcb0601ec03e53e061a7c4ab85a5847387c07c6a
superwf <superwf@gmail.com>
2018-01-10 14:13:20 +0800

    shorten refresh time

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
-const cp = require('child_process')
-const path = require('path')
-const fs = require('fs')
-const log4js = require('log4js')
-const logger = log4js.getLogger()
-const chalk = require('chalk')
-const freeShadowsocks = require('free-shadowsocks')
+const cp = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const log4js = require("log4js");
+const logger = log4js.getLogger();
+const chalk = require("chalk");
+const freeShadowsocks = require("free-shadowsocks");
 
 log4js.configure({
   appenders: {
-    everything: {type: 'file', filename: 'ss.log'},
+    everything: { type: "file", filename: "ss.log" }
   },
   categories: {
-    default: {appenders: ['everything'], level: 'debug'},
-  },
-})
+    default: { appenders: ["everything"], level: "debug" }
+  }
+});
 
-const configPath = path.join(__dirname, 'config.json')
-const pidPath = path.join(__dirname, 'pid')
+const configPath = path.join(__dirname, "config.json");
+const pidPath = path.join(__dirname, "pid");
 
-const log = console.log
+const log = console.log;
 
 function readConfig() {
   return new Promise(resolve => {
-    log(chalk.green('start read config.json'))
+    log(chalk.green("start read config.json"));
     fs.readFile(configPath, (err, data) => {
       if (err) {
-        reject(err)
-        return
+        reject(err);
+        return;
       }
-      resolve(JSON.parse(data))
-    })
-  })
+      resolve(JSON.parse(data));
+    });
+  });
 }
 
 function writeConfig() {
-  log(chalk.green('start read from ishadowx'))
+  log(chalk.green("start read from ishadowx"));
   return freeShadowsocks().then(servers => {
     // use second server
-    const server = servers[1]
-    log(chalk.cyan('user server config', server))
+    const server = servers[1];
+    log(chalk.cyan("user server config", server));
     return readConfig().then(config => {
-      Object.assign(config, server)
+      Object.assign(config, server);
       return new Promise((resolve, reject) => {
-        log(chalk.cyan('start write config.json'))
+        log(chalk.cyan("start write config.json"));
         fs.writeFile(configPath, JSON.stringify(config, null, 2), err => {
           if (err) {
-            reject(err)
+            reject(err);
           }
-          resolve()
-        })
-      })
-    })
-  })
+          resolve();
+        });
+      });
+    });
+  });
 }
 
 // sslocal process
-let ss
+let ss;
 function run() {
   writeConfig()
     .then(() => {
-      log(chalk.green('starting sslocal'))
-      ss = cp.spawn('sslocal', ['-c', configPath])
-      ss.stderr.on('data', err => {
-        logger.error(String(err))
-      })
+      log(chalk.green("starting sslocal"));
+      ss = cp.spawn("sslocal", ["-c", configPath]);
+      ss.stderr.on("data", err => {
+        logger.error(String(err));
+      });
     })
     .catch(err => {
-      log(chalk.red(err))
-      logger.debug(String(err))
-    })
-  fs.writeFile(pidPath, process.pid, log)
+      log(chalk.red(err));
+      logger.debug(String(err));
+    });
+  fs.writeFile(pidPath, process.pid, log);
 
-  // 3 hours restart
+  // 1 hours restart
   setTimeout(() => {
-    ss.kill()
-    run()
-  }, 1000 * 60 * 60 * 3)
+    ss.kill();
+    run();
+  }, 1000 * 60 * 60 * 1);
 }
 
 function killss() {
   if (ss) {
-    ss.kill()
+    ss.kill();
   }
-  process.exit()
+  process.exit();
 }
 
-process.on('SIGINT', killss)
-process.on('exit', killss)
-process.on('SIGUSER1', killss)
-process.on('uncaughtException', killss)
+process.on("SIGINT", killss);
+process.on("exit", killss);
+process.on("SIGUSER1", killss);
+process.on("uncaughtException", killss);
 
-run()
+run();
